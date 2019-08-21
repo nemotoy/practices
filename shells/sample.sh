9b653767b671ba99551a96d9add007e150dc8342
yuma-nemoto <yuma-nemoto@dac.co.jp>
2019-08-21 10:47:57 +0900

    Add a function that parses JSON from a file

--- shells/sample.sh
+++ shells/sample.sh
@@ -5,7 +5,19 @@ set -euo pipefail
 parse_json(){
     local current_path; current_path="$(cd "$(dirname "$0")"; pwd)"
     file="${current_path}/sample.json"
-    echo "${file}"
+    # JSONファイルから'[]'を削除し、キー'name'に該当する文字列を出力する。valuesは半角スペース区切りで出力する。
+    local ss; ss=$(< "${file}" jq -r '.[] | .name + " " + (.values | join(" "))')
+    # 空行をセットする
+    IFS=$'\n'
+    for line in ${ss}; do
+        # 配列rawに文字列lineを半角スペース区切りでセットする。
+        IFS=' ' read -a raw <<< "${line}"
+        name="${raw[0]}"
+        values=("${raw[@]:1}")
+        echo name: "${name}", values: "${values[@]}"
+    done
+    # IFSのリセットする
+    unset IFS
 }
 
 parse_json
